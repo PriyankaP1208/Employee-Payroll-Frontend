a8d92129e40a5b9331a19c1ad0522eb61673d42a
PriyankaP1208 <ppriyapatil1208@gmail.com>
2021-09-09 13:34:52 +0530

    feat:added validation for login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,30 +2,56 @@ import React from 'react';
 import {Link} from 'react-router-dom';
 import {Avatar, Button, Grid, Paper, TextField, Typography} from '@material-ui/core';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import {Formik, Form, Field, ErrorMessage} from 'formik';
+import * as Yup from 'yup';
 
 const Login=()=>{
-    const paperstyle = {padding:'30px 20px', height:'50vh', width:300, margin:'100px auto'}
-    const avatarStyle={backgroundColor:'#99db49'}
-    const btnStyle = {margin:'20px 0'}
+    const paperstyle = {padding:'30px 20px', height:'50vh', width:300, margin:'100px auto'};
+    const avatarStyle={backgroundColor:'#99db49'};
+    const btnStyle = {margin:'20px 0'};
+    const initialValues={
+        email:'',
+        password:''
+    };
+
+    const validationSchema = Yup.object().shape({
+        email: Yup.string().email('Please enter valid email id').required('Required'),
+        password: Yup.string().min(8, 'Enter valid password')
+    });
+
+    const onSubmit=(values)=>{
+        console.log(values)
+    }
 
     return(
         <Grid >
             <Paper elevation={10} style={paperstyle}>
                 <Grid align='center'>
                     <Avatar style={avatarStyle}><LockOutlinedIcon/></Avatar>
-                <h2>Sign In</h2>
+                    <h2>Sign In</h2>
                 </Grid>
-                <TextField label="Email" placeholder="enter emailid" fullWidth required/>
-                <TextField label="Password" placeholder="enter password" type='password' fullWidth required/>
-                <Button style={btnStyle} type='submit' color='primary' variant='contained' fullWidth>Sign In</Button>
-                
-                <Typography>
-                    Do you have an account?
-                    <Link to='/register'> Create Account</Link>
-                </Typography>
+
+                <Formik initialValues={initialValues} onSubmit={onSubmit} validationSchema={validationSchema}>
+                    {(props)=>(
+                        <Form>
+                            <Field as={TextField} label="Email" name="email" 
+                                placeholder="enter emailid" fullWidth required 
+                                helperText={<ErrorMessage name="email"/>}/>
+                            <Field as={TextField} label="Password" name="password" 
+                                placeholder="enter password" type='password' fullWidth required
+                                helperText={<ErrorMessage name="password"/>}/>
+                            <Button style={btnStyle} type='submit' color='primary' variant='contained' fullWidth>Sign In</Button>
+                            
+                            <Typography>
+                                Do you have an account?
+                                <Link to='/register'> Create Account</Link>
+                            </Typography>
+                        </Form>
+                    )}
+                </Formik>
             </Paper>
         </Grid>
-    )
-}
+    );
+};
 
 export default Login;
