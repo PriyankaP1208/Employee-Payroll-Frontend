fc1573761b9d2fad0bb427508ab5efa5d941002c
PriyankaP1208 <ppriyapatil1208@gmail.com>
2021-09-19 09:33:02 +0530

    feat:added dashboard page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import LogIn from './pages/login/Login.js';
 import SignUp from './pages/register/Register.js';
+import Dashboard from './components/dashboard';
+import { Router } from '@material-ui/icons';
 
 function App() {
   return (
@@ -8,7 +10,8 @@ function App() {
       <div className="App">
       <Switch>
         <Route exact path="/login" component={LogIn}/>
-        <Route exact path="/register" component={SignUp}/>
+        <Route path="/register" component={SignUp}/>
+        <Router path="/dashboard" component={Dashboard}/>
       </Switch>
       </div>
     </BrowserRouter>
