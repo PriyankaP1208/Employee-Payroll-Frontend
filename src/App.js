1888d96d456ec5bad02a3b15f7be838b665e2dc7
PriyankaP1208 <ppriyapatil1208@gmail.com>
2021-09-26 23:29:24 +0530

    feat:added lazy loading

--- src/App.js
+++ src/App.js
@@ -2,22 +2,24 @@ import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import React from 'react';
 import LogIn from './pages/login/Login.js';
 import SignUp from './pages/register/Register.js';
-import Dashboard from './pages/dashboard/dashboard';
+//import Dashboard from './pages/dashboard/dashboard';
 import ErrorPage from "./components/errorPage";
 import ProtectedRoute from "./components/protectedRoute";
+
+const LazyDashboard = React.lazy(() => import("./pages/dashboard/dashboard"));
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
-
         <Switch>
+        <React.Suspense fallback="Loading...">
           <Route exact path="/login" component={LogIn}/>
           <Route path="/register" component={SignUp}/>
-          <ProtectedRoute path="/dashboard" component={Dashboard}/>
+          <ProtectedRoute path="/dashboard" component={LazyDashboard}/>
+          </React.Suspense>
           <Route component={ErrorPage} />
         </Switch>
-        
       </div>
     </BrowserRouter>
   );
